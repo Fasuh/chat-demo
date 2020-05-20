57cf82433b96c796e657a7195ddc31ba8660ff0e
bbkk9@wp.pl <bbkk9@wp.pl>
2020-05-20 10:25:27 +0200

    authentication

--- flutter/common/lib/model/model.dart
+++ flutter/common/lib/model/model.dart
@@ -3,8 +3,12 @@ import 'package:angel_orm/src/query.dart';
 import 'package:angel_model/angel_model.dart';
 import 'package:angel_serialize/angel_serialize.dart';
 import 'package:angel_migration/angel_migration.dart';
+import 'package:enum_to_string/enum_to_string.dart';
 
 part 'model.g.dart';
 part 'test.dart';
 part 'user.dart';
+part 'error.dart';
 part 'client.dart';
+part 'refresh_token.dart';
+part 'auth_credentials.dart';
---
+++ flutter/new_server/lib/extensions/model.dart
@@ -0,0 +1,5 @@
+import 'package:angel_framework/angel_framework.dart';
+
+extension query on Query {
+
+}
