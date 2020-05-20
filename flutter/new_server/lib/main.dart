341c6fe3a80110707ddfe0c3ff753d7027f6f264
bbkk9@wp.pl <bbkk9@wp.pl>
2020-05-20 12:56:23 +0200

    angel_production

--- flutter/new_server/lib/main.dart
+++ flutter/new_server/lib/main.dart
@@ -1,36 +1,28 @@
 import 'package:angel_configuration/angel_configuration.dart';
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_container/mirrors.dart';
-import 'package:angel_framework/http.dart';
+import 'package:angel_production/angel_production.dart';
 import 'package:angel_websocket/server.dart';
 import 'package:hello/authentication/auth.dart';
 import 'package:hello/controller/auth_controller.dart';
 import 'package:hello/controller/chat_controller.dart';
 import 'package:hello/controller/test_controller.dart';
-import 'package:logging/logging.dart';
-import 'services.dart';
 import 'package:file/local.dart';
+import 'package:hello/services.dart';
 
-main() async {
-  var app = Angel(reflector: MirrorsReflector());
-  var ws = AngelWebSocket(app);
-  var http = AngelHttp(app);
+main(List<String> args) => Runner('pom', configureServer, reflector: MirrorsReflector()).run(args);
+
+Future configureServer(Angel app) async {
   var auth = AuthServer();
   var fs = const LocalFileSystem();
+  final ws = AngelWebSocket(app);
 
   await app.configure(configuration(fs));
   await app.configure(ws.configureServer);
   await app.configure(ChatController(ws).configureServer);
   await app.configure(AuthController(auth).configureServer);
   await app.configure(TestController().configureServer);
-  await app.configure(configureServer);
+  await app.configure(configureServerORM);
 
-  app.all("/ws", ws.handleRequest);
-
-  await http.startServer('localhost', 8000);
-  app.logger = Logger('server');
-  Logger.root.onRecord.listen((record) {
-    print('${record.level.name}: ${record.time}: ${record.message}');
-  });
-  app.logger.log(Level.INFO, "started");
+//  app.all("/ws", ws.handleRequest); TODO - test
 }
--- flutter/new_server/lib/services.dart
+++ flutter/new_server/lib/services.dart
@@ -1,9 +1,8 @@
 import 'package:angel_framework/angel_framework.dart';
-import 'package:angel_orm/angel_orm.dart';
 import 'package:angel_orm_postgres/angel_orm_postgres.dart';
 import 'package:hello/init_postgres.dart';
 
-Future<void> configureServer(Angel app) async {
+Future<void> configureServerORM(Angel app) async {
   var connection = connectToPostgres(app.configuration);
   await connection.open();
 
@@ -11,4 +10,3 @@ Future<void> configureServer(Angel app) async {
     ..container.registerSingleton<PostgreSqlExecutor>(PostgreSqlExecutor(connection))
     ..shutdownHooks.add((_) => connection.close());
 }
-
